f10ecfe57372a3dd3893ea3d9c0d7bbf80f26446
north110 <b20125@students.iitmandi.ac.in>
2023-01-07 19:14:39 +0530

    First Commit

---
+++ First Project/First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjectApp.swift
+//  First Project
+//
+//  Created by Rehan Saiyad on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
